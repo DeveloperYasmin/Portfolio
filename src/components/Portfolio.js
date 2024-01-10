f6b8ca44906c71cbe398132481bf43ab9ec726f6
DeveloperYasmin <banuyasmin96@gmail.com>
2024-01-10 12:48:38 +0530

    port

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -58,7 +58,6 @@ const Portfolio = () => {
             ghpage:"https://developeryasmin.github.com"
 
         }
-        
     ]
   return (
     <Section title="Portfolio 📝" subtitle="These are all the projects that I have worked on.Some of them I have worked before I gained some experience.So go gentle on them.">
